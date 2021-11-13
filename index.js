78a4979eb795a3dc6a257fb07a08427c5e0ce45e
Aidan Gawronski <aidangawronski@gmail.com>
2021-11-13 00:05:32 -0800

    update details

--- index.js
+++ index.js
@@ -96,10 +96,10 @@ function replaceUrls(text) {
 }
 
 Typer.speed = 3;
-Typer.file = 'CodeNerve.txt';
+Typer.file = 'AidanGawronski.txt';
 Typer.init();
 
-var timer = setInterval('t();', 30);
+var timer = setInterval('t();', 10);
 function t() {
   Typer.addText({ keyCode: 123748 });
 
